a060c87386e9df5a698c512fc4148546cd5dd504
ThanhDuy28 <duy0303nt@gmail.com>
2026-03-08 11:25:58 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -85,5 +85,6 @@ def predict():
 
 # --- CHAY SERVER ---
 if __name__ == '__main__':
-    # Chay tren cong 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Render se tu cap phat Port thong qua bien moi truong
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
